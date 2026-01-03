a254e6ffb54477e9c51578fe5f3486990cb68759
Mircea Niculae <mircea.bogdan0517@gmail.com>
2026-01-03 15:19:43 +0200

    First Commit

---
+++ Ocean.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Ocean : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Succes");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
